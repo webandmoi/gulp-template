ee385639ff3d6952fe8a42cd7f5e1b28c805d2d4
webandmoi <mugibatake@hotmail.co.jp>
2019-07-17 15:40:19 +0900

    sorce format

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp        = require('gulp');
+const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const browserSync = require("browser-sync");
-const sass        = require('gulp-sass');
+const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
-const minifycss = require('gulp-minify-css');
+const minifycss = require("gulp-minify-css");
 const sourcemaps = require("gulp-sourcemaps");
-const babel        = require('gulp-babel');
+const babel = require("gulp-babel");
 
 const srcPath = "src/";
 const distPath = "dist/";
@@ -13,80 +13,80 @@ const distPath = "dist/";
 /* ========================================
 	watch
 ======================================== */
-gulp.task('watch', () => {
-	gulp.watch(srcPath + "html/*.html",['copy','reload']);
-	gulp.watch([srcPath + 'sass/**'], function(){
-		gulp.start(['sass']);
-	});
-  gulp.watch(srcPath + 'js/**', ['copy','babel']);
-	gulp.watch(srcPath + "img/**/*",['copy','reload']);
+gulp.task("watch", () => {
+  gulp.watch(srcPath + "html/*.html", ["copy", "reload"]);
+  gulp.watch([srcPath + "sass/**"], function() {
+    gulp.start(["sass"]);
+  });
+  gulp.watch(srcPath + "js/**", ["copy", "babel"]);
+  gulp.watch(srcPath + "img/**/*", ["copy", "reload"]);
 });
 
 /* ========================================
 	browser-sync
 ======================================== */
-gulp.task('browser-sync', () => {
-	browserSync({
-	  port: 8888,
-		open: "external",
-		ghostMode: false,
+gulp.task("browser-sync", () => {
+  browserSync({
+    port: 8888,
+    open: "external",
+    ghostMode: false,
     browser: ["google chrome"],
     server: {
-			baseDir: distPath
-     ,index: "index.html"
-		}
-	});
+      baseDir: distPath,
+      index: "index.html"
+    }
+  });
 });
 
 /* ========================================
 	sass
 ======================================== */
 gulp.task("sass", () => {
-	gulp.src(srcPath + "sass/bundle.scss")
-  .pipe(plumber())
-  .pipe(autoprefixer())
-  .pipe(sourcemaps.init())
-  .pipe(sass())
-  .pipe(minifycss())
-  .pipe(sourcemaps.write())
-	.pipe(gulp.dest(distPath + "css"))
-	.pipe(browserSync.stream())
+  gulp
+    .src(srcPath + "sass/bundle.scss")
+    .pipe(plumber())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(minifycss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(distPath + "css"))
+    .pipe(browserSync.stream());
 });
 
 /* ========================================
 	babel
 ======================================== */
-gulp.task('babel', () => {
-  gulp.src([ srcPath + 'js/**/*' ])
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest(distPath + 'js/'))
+gulp.task("babel", () => {
+  gulp
+    .src([srcPath + "js/**/*"])
+    .pipe(
+      babel({
+        presets: ["es2015"]
+      })
+    )
+    .pipe(gulp.dest(distPath + "js/"));
 });
 
 /* ========================================
   copy
 ======================================== */
-gulp.task('copy', () => {
-  gulp.src([ srcPath + 'html/**/*' ])
-	.pipe(gulp.dest(distPath));
-	
-  gulp.src([ srcPath + 'img/**/*' ])
-	.pipe(gulp.dest(distPath + 'img'));
+gulp.task("copy", () => {
+  gulp.src([srcPath + "html/**/*"]).pipe(gulp.dest(distPath));
 
-  gulp.src([ srcPath + 'js/lib/**/*' ])
-	.pipe(gulp.dest(distPath + 'js/lib'));
+  gulp.src([srcPath + "img/**/*"]).pipe(gulp.dest(distPath + "img"));
+
+  gulp.src([srcPath + "js/lib/**/*"]).pipe(gulp.dest(distPath + "js/lib"));
 });
 
 /* ========================================
   reload
 ======================================== */
-gulp.task('reload', () => {
-	browserSync.reload();
+gulp.task("reload", () => {
+  browserSync.reload();
 });
 
 /* ========================================
   start
 ======================================== */
-gulp.task('default', ['copy', 'sass', 'babel', 'browser-sync', 'watch']);
-
+gulp.task("default", ["copy", "sass", "babel", "browser-sync", "watch"]);
